cb063dc4eb55c5a5cc3a918f4ccdfa483f8e199f
Dilton D'souza <84121216+dsouzadilton@users.noreply.github.com>
2022-07-13 16:13:40 +0530

    Update client_chat.py

--- client_chat.py
+++ client_chat.py
@@ -1,36 +1,18 @@
 import socket
-import os
 
-s = socket.socket()		
-r, w = os.pipe()
-
-port = 12345				
+s = socket.socket()
+port = 12345
 
 s.connect(('127.0.0.1', port))
-data = s.recv(1024).decode()
+status = 'True'
+while status:
+        text = s.recv(1024).decode()
+        if text != 'over':
+                print("Mr. Blue: ", text)
+                val = input("You: ")
+                s.send(val.encode())
+        else :
+                status = 'False'
+                print("Terminating Connection...")
 s.close()
-
-def isPalindrome(str):
-            if str == str[::-1]:
-                print("It's a Pallindrome")
-            else :
-                print("It's not a Pallindrome")
-
-def parent_child():
-    p2 = os.fork()
-    if p2 == 0:
-        intel[1] = intel[1].upper()
-        print("Second part to upper: ",intel[1])
-    elif p2 > 0:
-        p1 = os.fork()
-        if p1 == 0:
-            isPalindrome(str)
-        elif p1 > 0:
-            p1 = os.fork()
-        print("Child process and id is : ", os.getpid())
-        
-print("Recieved data: ",data)
-intel = []
-intel = data.split()
-str = intel[0]
-parent_child()
+print("Connection Terminated")
